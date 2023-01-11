ee6bc5fcbf96aace4538fc056c18c02f9c043bde
Rishabh Singh <rishabhsingh@medtronic.com>
2023-01-11 21:04:20 +0530

    notes api

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const noteRouter = require("./routes/noteRoutes");
+const userRouter = require("./routes/userRoutes");
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+dotenv.config()
+
+const mongoose = require("mongoose");
+
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+
+app.get("/",(req,res)=>{
+    res.send("Hello");
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server started on port no. "+PORT);
+        });
+})
+.catch((error)=>{
+console.log(error)
+})
+
